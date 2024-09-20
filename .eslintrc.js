c812eb216eca4c0498e7764a2c711e9471af3784
Jack Gluskin <jack@cloudquery.io>
2024-09-20 11:07:30 +0100

    Fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
+    tsconfigRootDir: '.',
     ecmaVersion: 12,
     sourceType: 'module',
   },
