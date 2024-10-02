7cc52043c70647eb92e76e8a4f41fea9a483f549
Herman Schaaf <hermanschaaf@gmail.com>
2024-10-02 16:04:56 +0100

    fix: Type plugin label in tests, rather than name (#67)

--- src/e2e-utils/plugin-ui-e2e-helpers.ts
+++ src/e2e-utils/plugin-ui-e2e-helpers.ts
@@ -7,8 +7,18 @@ export const getPersistentName = () => `name-${Math.random().toString(36).slice(
 type CreatePluginControlOpts = {
   page: Page;
   kind: 'source' | 'destination';
-  pluginName: string;
+  /**
+   * @deprecated - This property is no longer used in the test functions. It is the api identifier
+   * for the plugin (ie. `google-search-console`)
+   */
+  pluginName?: string;
+  /**
+   * User readable label of the plugin (ie. 'Google Search Console' for 'google-search-console')
+   */
   pluginLabel: string;
+  /**
+   * Desired user input for the `Source name` or `Destination name` input field.
+   */
   pluginNewName: string;
   fillFieldsSteps?: (iframeElement: Frame) => Promise<void>;
 };
@@ -39,7 +49,6 @@ export const login = async (page: Page) => {
 export const createPlugin = async ({
   page,
   kind,
-  pluginName,
   pluginNewName,
   pluginLabel,
   fillFieldsSteps,
@@ -49,7 +58,7 @@ export const createPlugin = async ({
 
   await expect(page.getByRole('heading', { name: `Create a ${kind}` })).toBeVisible();
 
-  await fillInput(page, 'input[type="text"]', pluginName);
+  await fillInput(page, 'input[type="text"]', pluginLabel);
 
   await click(page, page.getByRole('button', { name: pluginLabel }));
 
@@ -82,7 +91,6 @@ export const editPlugin = async ({
   page,
   kind,
   pluginNewName,
-  pluginName,
   pluginLabel,
   fillFieldsSteps,
   pluginUrl,
@@ -92,7 +100,7 @@ export const editPlugin = async ({
 
   await expect(page.getByRole('heading', { name: `Create a ${kind}` })).toBeVisible();
 
-  await fillInput(page, 'input[type="text"]', pluginName);
+  await fillInput(page, 'input[type="text"]', pluginLabel);
 
   await click(page, page.getByRole('button', { name: pluginLabel }));
   await expect(page.getByText('Previewing')).toBeVisible();
@@ -132,7 +140,6 @@ export const deletePlugin = async ({
   page,
   kind,
   pluginNewName,
-  pluginName,
   pluginLabel,
   pluginUrl,
 }: EditPluginControlOpts) => {
@@ -141,7 +148,7 @@ export const deletePlugin = async ({
 
   await expect(page.getByRole('heading', { name: `Create a ${kind}` })).toBeVisible();
 
-  await fillInput(page, 'input[type="text"]', pluginName);
+  await fillInput(page, 'input[type="text"]', pluginLabel);
 
   await click(page, page.getByRole('button', { name: pluginLabel }));
   await expect(page.getByText('Previewing')).toBeVisible();
