58de2a69bb530b48ada98f5d5f1e9dd2e3906637
Jack Gluskin <jack@cloudquery.io>
2024-10-07 08:18:39 +0100

    Add missing export

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -18,3 +18,4 @@ export { getEnabledTablesObject } from './getEnabledTablesObject';
 export { escapeSingleQuotesAndBackslashes } from './escapeSingleQuotesAndBackslashes';
 export { getFieldHelperText } from './getFieldHelperText';
 export { getYupValidationResolver, resetYupDefaultErrorMessages } from './getYupValidationResolver';
+export { finishAuthConnectorAuthentication } from './finishAuthConnectorAuthentication';
