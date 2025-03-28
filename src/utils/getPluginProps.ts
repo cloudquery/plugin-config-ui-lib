cb5fe5006467b73f0872470421dba42a1ba6b967
Jack Gluskin <jack@cloudquery.io>
2025-03-28 16:07:23 +0000

    Make plugin version dynamic

--- src/utils/getPluginProps.ts
+++ src/utils/getPluginProps.ts
@@ -12,6 +12,6 @@ export function getPluginProps() {
     team: process.env.REACT_APP_PLUGIN_TEAM,
     kind: process.env.REACT_APP_PLUGIN_KIND,
     name: process.env.REACT_APP_PLUGIN_NAME,
-    version: process.env.REACT_APP_PLUGIN_VERSION,
+    version: (window as any).REACT_APP_PLUGIN_VERSION || process.env.REACT_APP_PLUGIN_VERSION,
   };
 }
