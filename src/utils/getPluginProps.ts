08d42b4ae836682849c8f0d28f366f3fb0ff633d
Jack Gluskin <jack@cloudquery.io>
2025-03-28 17:00:42 +0000

    Add preview

--- src/utils/getPluginProps.ts
+++ src/utils/getPluginProps.ts
@@ -12,7 +12,7 @@ export function getPluginProps() {
     team: process.env.REACT_APP_PLUGIN_TEAM,
     kind: process.env.REACT_APP_PLUGIN_KIND,
     name: process.env.REACT_APP_PLUGIN_NAME,
-    version: process.env.REACT_APP_IS_REVIEW
+    version: process.env.REACT_APP_IS_PREVIEW
       ? (window as any).REACT_APP_PLUGIN_VERSION || process.env.REACT_APP_PLUGIN_VERSION
       : process.env.REACT_APP_PLUGIN_VERSION,
   };
